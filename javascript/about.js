c9282becf67ca6cd2198387d18e541a764eeecea
Roberto Gomez <roberto77@gmail.com>
2022-04-23 11:31:55 -0500

    javascript basics

---
+++ javascript/about.js
@@ -0,0 +1,3 @@
+document.getElementById("course").innerHTML = "MIS 3313 Web Analysis and Design";
+document.getElementById("term").innerHTML = "Spring 2022";
+document.getElementById("developers").innerHTML = "Web Developers";
